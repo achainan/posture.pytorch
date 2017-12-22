2453e239f15892bd2d042a76e6e67c65c3e10688
Ajay Chainani <ajay.chainani@gmail.com>
2017-12-22 12:01:07 -0500

    update constants

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 """While in development instead of args using a constants file"""
 
 num_epochs = 2000
-train_batch_size = 27
-val_batch_size = 27
+train_batch_size = 76
+val_batch_size = 12
 learning_rate = 0.001
-num_workers = 4
+num_workers = 10
 scale = .4
 default_width = 670
 default_height = 760
-save_interval = 10
-print_freq = 5
+save_interval = 20
+print_freq = 10
