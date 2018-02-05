bf53208a4a1f146e4f7a409cd42239be1de17556
Ajay Chainani <ajay.chainani@gmail.com>
2018-02-05 13:16:19 -0500

    Fix convolution calcuation bug

--- calculations.py
+++ calculations.py
@@ -3,12 +3,12 @@
 
 def conv_dim(width, height, padding, filter_size, stride):
     # (n + 2p - f)/ s + 1
-    output_width = (width + 2 * padding - filter_size) / stride + 1
-    output_height = (height + 2 * padding - filter_size) / stride + 1
-    return output_width, output_height
+    output_width = (width + 2.0 * padding - filter_size) / stride * 1.0 + 1
+    output_height = (height + 2.0 * padding - filter_size) / stride * 1.0 + 1
+    return int(output_width), int(output_height)
 
 
 def max_pool_dim(width, height, v):
-    output_width = width / v
-    output_width = height / v
-    return output_width, output_width
+    output_width = width / v * 1.0
+    output_height = height / v * 1.0
+    return int(output_width), int(output_height)
