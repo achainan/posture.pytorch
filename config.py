d57979e84f852d0e3c80bc8b8d947e63ba25c194
Ajay Chainani <ajay.chainani@gmail.com>
2018-02-05 20:32:45 -0500

    Create config.py file

---
+++ config.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Train the pose estimation model.')
+parser.add_argument('--num_epochs', type=int, default=8000)
+parser.add_argument('--csv_dir', type=str, default='B/')
+parser.add_argument('--root_dir', type=str, default='B/')
+parser.add_argument('--input_height', type=int, default=128)
+parser.add_argument('--model_name', type=str, default='posture')
+parser.add_argument('--display_freq', type=int, default=20, help="Save preview to tensorboard ever X epochs")
+parser.add_argument('--train_batch_size', type=int, default=120)
+parser.add_argument('--val_batch_size', type=int, default=12)
+
+display_parser = parser.add_mutually_exclusive_group(required=False)
+display_parser.add_argument('--display', dest='display', action='store_true')
+display_parser.add_argument('--no-display', dest='display', action='store_false')
+parser.set_defaults(display=True)
+
+normalize_parser = parser.add_mutually_exclusive_group(required=False)
+normalize_parser.add_argument('--normalize', dest='normalize', action='store_true')
+normalize_parser.add_argument('--no-normalization', dest='normalize', action='store_false')
+parser.set_defaults(normalize=True)
+
+cache_parser = parser.add_mutually_exclusive_group(required=False)
+cache_parser.add_argument('--cached', dest='cached', action='store_true')
+cache_parser.add_argument('--no-cached', dest='cached', action='store_false')
+parser.set_defaults(cached=False)
+
+args = parser.parse_args()
--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 import numpy as np
-import argparse
 
 import models
 import constants
@@ -15,33 +14,7 @@ from dataset import load_dataset, Normalize
 from tensorboardX import SummaryWriter
 from third_party import AverageMeter
 from normalization import normalization_values
-
-parser = argparse.ArgumentParser(description='Train the pose estimation model.')
-parser.add_argument('--num_epochs', type=int, default=8000)
-parser.add_argument('--csv_dir', type=str, default='B/')
-parser.add_argument('--root_dir', type=str, default='B/')
-parser.add_argument('--input_height', type=int, default=128)
-parser.add_argument('--model_name', type=str, default='posture')
-parser.add_argument('--display_freq', type=int, default=20, help="Save preview to tensorboard ever X epochs")
-parser.add_argument('--train_batch_size', type=int, default=120)
-parser.add_argument('--val_batch_size', type=int, default=12)
-
-display_parser = parser.add_mutually_exclusive_group(required=False)
-display_parser.add_argument('--display', dest='display', action='store_true')
-display_parser.add_argument('--no-display', dest='display', action='store_false')
-parser.set_defaults(display=True)
-
-normalize_parser = parser.add_mutually_exclusive_group(required=False)
-normalize_parser.add_argument('--normalize', dest='normalize', action='store_true')
-normalize_parser.add_argument('--no-normalization', dest='normalize', action='store_false')
-parser.set_defaults(normalize=True)
-
-cache_parser = parser.add_mutually_exclusive_group(required=False)
-cache_parser.add_argument('--cached', dest='cached', action='store_true')
-cache_parser.add_argument('--no-cached', dest='cached', action='store_false')
-parser.set_defaults(cached=False)
-
-args = parser.parse_args()
+from config import args
 
 cuda = torch.cuda.is_available()
 logger = SummaryWriter()
